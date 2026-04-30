f785699241cf948a68425c3e1d997f2568b04a48
pbacalgini <pbacalgini@gmail.com>
2026-04-30 08:26:08 -0300

    coisas_novas2

---
+++ desafio2.php
@@ -0,0 +1,7 @@
+<?php
+$contador = 1;
+while ($contador <= 10) {
+    echo "joao pedro matheus" .  "</p>";
+    $contador++;
+}
+?>
--- index.php
+++ index.php
@@ -32,5 +32,50 @@
     }
 ?>
 <a href="admin/produtos/cadastrar.php">Cadastrar Produto</a>
+<?php
+$contador = 1;
+while ($contador <= 5) {
+    echo "contagem" . $contador . "</p>";
+    $contador++;
+}
+?>
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    echo "contagem for: " . $i . "</p>";
+}
+?>
+<?php
+$frutas=array("maçã", "banana", "laranja");{
+    echo $frutas [0]. "<br>";
+    echo $frutas [1]. "<br>";
+}
+$produto = array(
+    "nome" => "Camiseta",
+    "preco" => 29.99,
+    "descricao" => "Camiseta de algodão"
+);
+$produto["preco"] = 24.99; // Atualizando o preço do produto    
+echo "Nome: " . $produto["nome"] . "<br>";
+echo "Preço: R$ " . number_format($produto["preco"], 2, ',', '.') . "<br>";
+
+
+$produtos = [
+    [
+        "id" => 1,
+        "nome" => "teclado mecanico",
+        "preco" => 250.99,
+        "estoque" => 10
+    ],
+    [
+        "id" => 2,
+        "nome" => "mouse gamer",
+        "preco" => 200.99,
+        "estoque" => 5
+    ]
+    
+    ];
+    echo $produtos[0]["nome"] . " - R$ " . number_format($produtos[0]["preco"], 2, ',', '.') . "<br>";
+    echo $produtos[1]["nome"] . " - R$ " . number_format($produtos[1]["preco"], 2, ',', '.') . "<br>";
+?>
 </body>
 </html>
